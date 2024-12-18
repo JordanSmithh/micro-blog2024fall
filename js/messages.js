e235da040193ca35c8800d6810314989cd845344
Jordan Smith <Jordansmithformally@gmail.com>
2024-12-17 19:00:49 -0500

    fixes

--- js/messages.js
+++ js/messages.js
@@ -14,29 +14,101 @@
     ]
   },
 */
+
+async function handleLike(postElement) {
+  console.dir(postElement.dataset.post_id, "hello")
+
+  const payload = JSON.stringify({ "postId": postElement.dataset.post_id });
+
+  const response = await fetch(
+    BASE_URL + "/api/likes/", {
+    method: "POST",
+    headers: headersWithAuth(),
+    body: payload
+  });
+  const object = await response.json();
+  if (object.statusCode == 400) {
+    alert("You already liked this post")
+  } else if (object._id) {
+    alert("Message Liked!")
+    window.location.reload();
+  } else {
+    alert("Error");
+    window.location.reload();
+  }
+}
+
+ async function handleUnLike(postElement) {
+  const username = localStorage.getItem("username");
+
+  const response = await fetch(
+    BASE_URL + "/api/posts/"+postElement.dataset.post_id , {
+    method: "GET",
+    headers: headersWithAuth()
+  });
+  const object = await response.json();
+  if(object.likes) {
+    object.likes.forEach(async (like) => {
+      if(like.username == username) {
+        console.log("found auth user")
+
+        const response = await fetch(
+          BASE_URL + "/api/likes/"+like._id , {
+          method: "DELETE",
+          headers: headersWithAuth()
+        });
+        const object = await response.json();
+        if(object.statusCode == 202) {
+          alert("You unliked the post");
+          window.location.reload();
+        }
+      }
+    })
+  } 
+
+}
+
 function getMessage(m) {
-    return `
+  return `
         <div data-post_id="${m._id}" class="message">
             FROM:  ${m.username}<br>\n    
             WHEN:  ${m.createdAt}<br>\n    
             TEXT:  ${m.text}<br>\n
             LIKES: ${m.likes.length}
+            <button class="btn-like">Like</button>
+            <button class="btn-unlike">Unlike</button>
         </div>
     `;
 }
+
+
+
 document.addEventListener("DOMContentLoaded", async () => {
 
-    const messages = await getMessageList();
+  const messages = await getMessageList();
+  const userToken = localStorage.getItem("token");
 
-    const userToken = localStorage.getItem("token");
-    
-    if(userToken) {
-      output.innerHTML = messages.map(getMessage).join("<hr>\n")
-    } else {
-      console.log("triggered")
-      alert("Please login first");
-      window.location.href="index.html";
-    }
+  if (userToken) {
+    output.innerHTML = messages.map(getMessage).join("<hr>\n")
+  } else {
+    console.log("triggered")
+    alert("Please login first");
+    window.location.href = "index.html";
+  }
 
+
+  const likeButtons = document.querySelectorAll(".btn-like");
+  likeButtons.forEach(likeButton => {
+    likeButton.addEventListener("click", function (event) {
+      handleLike(likeButton.parentElement);
+    })
+  })
+
+  const unlikeButtons = document.querySelectorAll(".btn-unlike");
+  unlikeButtons.forEach(unlike => {
+    unlike.addEventListener("click", function (event) {
+      handleUnLike(unlike.parentElement);
+    })
+  })
 
 });//end load
--- js/profile.js
+++ js/profile.js
@@ -1,20 +1,74 @@
-document.addEventListener("DOMContentLoaded", function(){
+// /api/users/{username}
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    const userToken = localStorage.getItem("token");
+    const username = localStorage.getItem("username");
     const logoutButton = document.getElementById("logout-btn");
+    const updateForm = document.getElementById("profile-form");
 
-    logoutButton.addEventListener("click", function() {
-        const userToken = localStorage.getItem("token");
-        const username = localStorage.getItem("username");
+    function displayUserData(userData){
+        if(userData) {
+            const userDataEl = document.getElementById("userdata");
+            userDataEl.innerHTML = `
+                <h3>${userData.username}</h3>
+                <p>${userData.bio}</p>
+            `;
+        }
+    }
 
-        if(userToken && username) {
+    // function to fetch user information
+    async function getUserData() {
+
+        const response = await fetch(BASE_URL + "/api/users/"+username, {
+            method: "GET",
+            headers: headersWithAuth(),
+        }); //end fetch
+
+        //TODO check for error response status codes
+        if (response.status != 200) {
+            console.log(response.status, response.statusText);
+            return response.statusText;
+        }
+        const object = await response.json(); //COnvert body to object
+        displayUserData(object);
+    }
+
+    getUserData();
+
+
+    logoutButton.addEventListener("click", function () {
+
+        if (userToken && username) {
             localStorage.removeItem("token");
             localStorage.removeItem("username");
 
             alert("User logged out successfully");
 
-            window.location.href="index.html";
+            window.location.href = "index.html";
         } else {
             alert("User is not logged in");
+        }
+    })
+
+    updateForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        const fullNameValue = document.getElementById("fullName").value;
+        const passwordValue = document.getElementById("password").value;
+        const bioValue = document.getElementById("bio").value;
+
+        const payload = JSON.stringify({ "password": passwordValue, "bio": bioValue, "fullName": fullNameValue });
+
+        const response = await fetch(
+            BASE_URL + "/api/users/" + username, {
+            method: "PUT",
+            headers: headersWithAuth(),
+            body: payload
+        });
+        const object = await response.json();
+        if(object.fullName) {
+            alert("User data updated successfully")
+            window.location.reload();
         }
     })
 
